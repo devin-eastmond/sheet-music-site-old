f7cccb547659c9983ae896bede497d32127cedfa
Devin Eastmond <devinator9876@gmail.com>
2020-10-17 15:20:19 -0600

    Functionality with viewing songs one at a time

---
+++ scripts/viewSong.js
@@ -0,0 +1,37 @@
+var isPlaying = false;
+
+window.onload = function() {
+  const urlParams = new URLSearchParams(window.location.search);
+  let song = urlParams.get('song');
+
+  loadSongInfo(song);
+}
+
+function loadSongInfo(song) {
+  if (song != null) {
+    document.getElementById("pdfLink").href = "images/" + song + ".pdf";
+    document.getElementById("imagePreview").src = "images/" + song + ".jpg";
+    document.getElementById("audioSource").src = "audio/" + song + ".mp3";
+    document.getElementById("myAudio").load();
+    document.getElementById("download").href = "images/" + song + ".pdf";
+
+    var songInfoJson = JSON.parse("songsInfo.json");
+    console.log(songInfoJson);
+  }
+}
+
+function playSong() {
+  isPlaying = !isPlaying;
+  var audio = document.getElementById("myAudio");
+  if (isPlaying) {
+    document.getElementById("playButton").querySelector('#buttonText').innerHTML = "Pause"
+    audio.play();
+  } else {
+    document.getElementById("playButton").querySelector('#buttonText').innerHTML = "Play Song"
+    audio.pause();
+  }
+}
+
+function downloadPDF() {
+  document.getElementById('download').click();
+}
